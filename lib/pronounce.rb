708ffe05aa53fc9e58d1588c93a704c4bbfa078f
Graham Wagener <graham.wagener@gmail.com>
2012-11-13 12:41:39 -0700

    Added syllabification by the Sonority Sequencing Principle.

--- lib/pronounce.rb
+++ lib/pronounce.rb
@@ -7,32 +7,64 @@ module Pronounce
       @pronouncation_dictionary ||= build_pronuciation_dictionary
       @pronouncation_dictionary[word.downcase]
     end
-    
+
     def symbols
-      File.read("#{DATA_DIR}/cmudict/cmudict.#{CMUDICT_VERSION}.symbols").
-           split("\r\n")
+      @symbols ||= File.read("#{DATA_DIR}/cmudict/cmudict.#{CMUDICT_VERSION}.symbols").
+                        split("\r\n")
     end
-    
+
     def phones
-      File.read("#{DATA_DIR}/cmudict/cmudict.#{CMUDICT_VERSION}.phones").
-           split("\n").
-           reduce({}){|phones, phone| phone, type = *phone.split("\t"); phones.merge({phone => type}) }
+      @phones ||= File.read("#{DATA_DIR}/cmudict/cmudict.#{CMUDICT_VERSION}.phones").
+                       split("\n").
+                       reduce({}){|phones, phone| phone, type = *phone.split("\t"); phones.merge({phone => type}) }
     end
 
     private
-  
+
     def build_pronuciation_dictionary
       dictionary = {}
-    
-      File.open("#{DATA_DIR}/cmudict/cmudict.#{CMUDICT_VERSION}") do |f|
-        f.readlines.each do |line|
-          word, *pron = line.strip.split(' ')
-          next unless word && !word.empty? && !word[/[^A-Z]+/]
-          dictionary[word.downcase] = pron
-        end
+
+      File.readlines("#{DATA_DIR}/cmudict/cmudict.#{CMUDICT_VERSION}").each do |line|
+        word, *pron = line.strip.split(' ')
+        next unless word && !word.empty? && !word[/[^A-Z]+/]
+        dictionary[word.downcase] = split_syllables(pron)
       end
-      
+
       dictionary
+    end
+
+    def split_syllables(word)
+      syllables = []
+      syllable = []
+      word.each_with_index do |phone, i|
+        if new_syllable?(word, i)
+          syllables << syllable
+          syllable = []
+        end
+        syllable << phone
+      end
+      syllables << syllable
+    end
+
+    def new_syllable?(word, index)
+      return false if index == 0
+
+      return false unless index < word.length - 1
+      sonority(word[index]) <= sonority(word[index+1]) && sonority(word[index]) < sonority(word[index-1])
+    end
+
+    def sonority(symbol)
+      @sonorance ||= {
+        'aspirate' => 0,
+        'stop' => 1,
+        'affricate' => 2,
+        'fricative' => 3,
+        'nasal' => 4,
+        'liquid' => 5,
+        'semivowel' => 6,
+        'vowel' => 7
+      }
+      @sonorance[self.phones[symbol[0..1]]]
     end
 
   end
