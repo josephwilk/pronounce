0b11d7ddac2b40ab41014a60feb71c9b15239a02
Graham Wagener <graham.wagener@gmail.com>
2013-05-30 23:00:13 +1200

    Further removed rule definitions from where they're run.

--- lib/syllable_rules.rb
+++ lib/syllable_rules.rb
@@ -1,10 +1,10 @@
 module Pronounce::SyllableRules
   def self.evaluate(context)
-    [:stressed_syllables_heavy, :disallow_ng_onset].each do |rule|
+    rules[:en].each do |rule|
       is_new_syllable = English.send rule, context
       return is_new_syllable unless is_new_syllable.nil?
     end
-    sonority_sequencing_principle context
+    send rules[:base], context
   end
 
   # Breaks syllables at the low point of sonority between vowels.
@@ -12,6 +12,15 @@ module Pronounce::SyllableRules
     return true if context.current_phone.syllabic? && !context.previous_phone_in_onset?
     return false if context.word_end?
     (context.current_phone < context.next_phone && context.current_phone <= context.previous_phone) || context.previous_phone_in_coda?
+  end
+
+  private
+
+  def self.rules
+    {
+      en: [:stressed_syllables_heavy, :disallow_ng_onset],
+      base: :sonority_sequencing_principle
+    }
   end
 
 end
